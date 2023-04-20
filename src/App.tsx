8d7238dd63f9a3a59b3137446f79b18b674ccbdf
YOOUMUU <yooumuu@gmail.com>
2023-04-20 20:07:58 +0800

    ts option interface setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <Alert>
-      Hello <strong>World</strong>
-    </Alert>
+    <div>
+      <Alert>
+        Hello <strong>World</strong>
+      </Alert>
+      <Button onClick={() => console.log("Clicked")}>My Button</Button>
+      <Button color="danger" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
+    </div>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button onClick={onClick} className={`btn btn-${color}`}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
