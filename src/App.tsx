42ee68dd101e62bc1b056bf2d17e8c1f42e76a6c
YOOUMUU <yooumuu@gmail.com>
2023-04-20 19:04:32 +0800

    ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import Message from "./Message";
+import ListGroup from "./components/LIstGroup";
 
 function App() {
-  return <Message />;
+  const cityItems = ["New York", "San Francisco", "Tokyo"];
+  const fruitItems = ["Apple", "Banana", "Orange"];
+
+  const hanleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <>
+      <ListGroup
+        items={cityItems}
+        heading="Cites"
+        onSelectItem={hanleSelectItem}
+      />
+      <ListGroup
+        items={fruitItems}
+        heading="Cites"
+        onSelectItem={hanleSelectItem}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/LIstGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+// {item:[], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  let [selectIndex, setSelectIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              index === selectIndex
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
