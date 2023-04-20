98766ac9ce768b270f3c65b86df9528e02467050
YOOUMUU <yooumuu@gmail.com>
2023-04-21 03:34:42 +0800

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import ListGroup from './components/ListGroup/index';
 import './App.css';
+import { CiAirportSign1 } from 'react-icons/ci';
 
 function App() {
   const items = ['New York', 'Tokyo', 'Beijing'];
-  return (
-    <>
-      <ListGroup items={items} heading='List' onSelectItem={() => {}} />
-    </>
-  );
+  return <CiAirportSign1 color="red" size="250px" />;
 }
 
 export default App;
