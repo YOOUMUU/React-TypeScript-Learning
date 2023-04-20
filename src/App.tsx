94cedd3e9b9f07492c7b2c85d71dc3b68bab77c5
YOOUMUU <yooumuu@gmail.com>
2023-04-20 19:34:59 +0800

    props children

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import ListGroup from "./components/LIstGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const cityItems = ["New York", "San Francisco", "Tokyo"];
-  const fruitItems = ["Apple", "Banana", "Orange"];
-
-  const hanleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <>
-      <ListGroup
-        items={cityItems}
-        heading="Cites"
-        onSelectItem={hanleSelectItem}
-      />
-      <ListGroup
-        items={fruitItems}
-        heading="Cites"
-        onSelectItem={hanleSelectItem}
-      />
-    </>
+    <Alert>
+      Hello <strong>World</strong>
+    </Alert>
   );
 }
 
