4010408f42941160cf769cf2795c29f2ca8f9102
YOOUMUU <yooumuu@gmail.com>
2023-04-23 02:24:39 +0800

    React-Hook-Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,13 +11,23 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input {...register('name')} type="text" className="form-control" />
+        <input
+          {...register('name')}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input {...register('age')} type="number" className="form-control" />
+        <input
+          {...register('age')}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
