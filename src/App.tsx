fd4be14123217bce7f2d539b454c61e1e4f4118e
YOOUMUU <yooumuu@gmail.com>
2023-04-23 16:35:06 +0800

    Building ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import Form from './components/Form';
-import './App.css';
+import { useState } from 'react';
+import ExpenseList from './components/expense-tracker/ExpenseList';
 
 function App() {
-  return <Form></Form>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'aaa', amount: 20, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 20, category: 'Utilities' },
+  ]);
+
+  return (
+    <ExpenseList
+      expenses={expenses}
+      onDelete={(id) =>
+        setExpenses(expenses.filter((expense) => expense.id !== id))
+      }
+    />
+  );
 }
 
 export default App;
