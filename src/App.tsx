99ea6a0a301c0edc2d449d93597bd21fd3975e33
YOOUMUU <yooumuu@gmail.com>
2023-04-25 01:00:25 +0800

    Building ExpenseList and Expense Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import { useState } from 'react';
-import ExpenseList from './components/expense-tracker/ExpenseList';
+import ExpenseList from './components/expense-tracker/components/ExpenseList';
+import ExpenseFilter from './components/expense-tracker/components/ExpenseFilter';
 
 function App() {
+  const [selectedCategory, setSelectCategory] = useState('');
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 20, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 20, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 20, category: 'Groceries' },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
-    <ExpenseList
-      expenses={expenses}
-      onDelete={(id) =>
-        setExpenses(expenses.filter((expense) => expense.id !== id))
-      }
-    />
+    <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
   );
 }
 
